bc612ea361bab93c926dd886d994ad34162d7b90
kyalorichard <kyalorichard11@gmail.com>
2025-12-03 13:49:54 -0300

    Delete pages/analytics.py

--- pages/analytics.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import pandas as pd
-
-st.title("📊 Analytics Tab")
-df = pd.read_csv("app/data/alerts.csv")
-
-st.subheader("Alerts by Country")
-st.bar_chart(df["Country"].value_counts())
