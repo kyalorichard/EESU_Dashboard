bbf7d79f85b0e5a4086f21bad2ee5864dfd947c0
kyalorichard <kyalorichard11@gmail.com>
2025-12-03 13:59:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -19,11 +19,7 @@ def load_data():
 df = load_data()
 
 # ---------------- DASHBOARD TITLE & LOGO AT TOP ----------------
-st.markdown("""
-    <h1 style='text-align:center; color:#660094; margin-bottom:5px;'>EU SEE Dashboard</h1>
-    <p style='text-align:center; color:gray; margin-top:0;'>EU SEE Dashboard</p>
-""", unsafe_allow_html=True)
-st.markdown("<hr>", unsafe_allow_html=True)
+
     
 
 # ---------------- GLOBAL SIDEBAR FILTERS ----------------
