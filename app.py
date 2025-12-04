70ebd8357af97ae2deedce554e447d9dfaf79096
kyalorichard <kyalorichard11@gmail.com>
2025-12-04 05:21:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -259,9 +259,9 @@ with tab3:
     r1c1, r1c2 = st.columns(2, gap="large")
     r2c1, r2c2 = st.columns(2, gap="large")
     with r1c1: st.plotly_chart(create_bar_chart(b3, x="alert-country", y="count", horizontal=True), use_container_width=True, key="tab3_chart1")
-    with r1c2: st.plotly_chart(create_bar_chart(b4, x="alert-type", y="count", horizontal=True), use_container_width=True, key="tab3_chart2")
+    with r1c2: st.plotly_chart(create_bar_chart(b4, x="alert-country", y="count", horizontal=True), use_container_width=True, key="tab3_chart2")
     with r2c1: st.plotly_chart(create_bar_chart(b1, x="alert-country", y="count"), use_container_width=True, key="tab3_chart3")
-    with r2c2: st.plotly_chart(create_bar_chart(b2, x="alert-country", y="count"), use_container_width=True, key="tab3_chart4")
+    with r2c2: st.plotly_chart(create_bar_chart(b2, x="alert-type", y="count"), use_container_width=True, key="tab3_chart4")
 
 # ---------------- TAB 4 ----------------
 with tab4:
