7e3fb2f80d17b756ed1e555b52f56eda47f647ec
kyalorichard <kyalorichard11@gmail.com>
2025-12-08 17:46:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -487,37 +487,26 @@ with tab2:
     summary_data = get_summary_data(active_tab)
     summary_data2 = summary_data[summary_data['alert-impact'] == "Negative"] 
     render_summary_cards(summary_data)
+    
+    actor_type_options = sorted(filtered_global['Actor of repression'].dropna().unique())
+    selected_actor_types = multiselect_with_all("Select Alert Type", actor_type_options, "selected_actor_types")
+    
+    subject_type_options = sorted(filtered_global['['Subject of repression'].dropna().unique())
+    selected_subject_types = multiselect_with_all("Select Subject Type", subject_type_options, "selected_subject_types")
+    
+    subject_mechanism_options = sorted(filtered_global['Mechanism of repression'].dropna().unique())
+    selected_mechanism_types = multiselect_with_all("Select Mechanism Type", mechanism_type_options, "selected_mechanism_types")
+    
+    event_type_options = sorted(filtered_global['Type of event'].dropna().unique())
+    selected_event_types = multiselect_with_all("Select Event Type", event_type_options, "selected_event_types")
 
-    tab2_actor = col1.multiselect_with_all(
-        "Actor of Repression",
-        options=sorted(filtered_global['Actor of repression'].dropna().unique()),
-        default=sorted(filtered_global['Actor of repression'].dropna().unique())
-    )
-    
-    tab2_subject = col2.multiselect_with_all(
-        "Subject of Repression",
-        options=sorted(filtered_global['Subject of repression'].dropna().unique()),
-        default=sorted(filtered_global['Subject of repression'].dropna().unique())
-    )
-
-    tab2_mechanism = col3.multiselect_with_all(
-        "Mechanism of Repression",
-        options=sorted(filtered_global['Mechanism of repression'].dropna().unique()),
-        default=sorted(filtered_global['Mechanism of repression'].dropna().unique())
-    )
-    
-    tab2_event_type = col4.multiselect_with_all(
-        "Type of Event",
-        options=sorted(filtered_global['Type of event'].dropna().unique()),
-        default=sorted(filtered_global['Type of event'].dropna().unique())
-    )
-    
+   
     # Filter data for Tab 2 using these selections
     summary_data = filtered_global[
-        (filtered_global['Actor of repression'].isin(tab2_actor)) &
-        (filtered_global['Subject of repression'].isin(tab2_subject)) &
-        (filtered_global['Mechanism of repression'].isin(tab2_mechanism)) &
-        (filtered_global['Type of event'].isin(tab2_event_type))
+        (filtered_global['Actor of repression'].isin(selected_actor_types)) &
+        (filtered_global['Subject of repression'].isin(selected_subject_types)) &
+        (filtered_global['Mechanism of repression'].isin(selected_mechanism_types)) &
+        (filtered_global['Type of event'].isin(selected_event_types))
     ]
     
     # Render summary cards
