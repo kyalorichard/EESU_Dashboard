52feb54621a28424bf9d8b15925b5e32eb2a08e8
kyalorichard <kyalorichard11@gmail.com>
2025-12-10 05:33:52 -0300

    Update data_preprocessing.py

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -20,6 +20,7 @@ SMTP_USER = os.getenv("SMTP_USER")
 SMTP_PASS = os.getenv("SMTP_PASS")
 SMTP_HOST = os.getenv("SMTP_HOST")
 SMTP_PORT = int(os.getenv("SMTP_PORT") or 587)
+######
 
 # ---------------- FILE PATHS -----------------
 INPUT_CSV = "data/raw_data.csv"
