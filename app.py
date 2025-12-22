6481b3f17b59fff7ebc8bb946a4a6f0733154c75
kyalorichard <kyalorichard11@gmail.com>
2025-12-22 02:46:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -316,6 +316,7 @@ def create_bar_chart(df, x, y, horizontal=False):
     fig.update_xaxes(title=None, showgrid=True, gridwidth=1, gridcolor='lightgray')
     fig.update_yaxes(title=None, showgrid=True, gridwidth=1, gridcolor='lightgray')
     fig.update_layout(height=height, margin=dict(l=120 if horizontal else 20, r=20, t=20, b=20))
+    fig.update_layout(title=title,title_x=0.5, margin=dict(l=10, r=10, t=40, b=10)
     return fig
 
 # ---------------- HORIZONTAL STACKED BAR ----------------
@@ -349,6 +350,8 @@ def create_h_stacked_bar(df, y, x="count", color_col="alert-impact", horizontal=
     fig.update_layout(barmode='stack', height=height, margin=dict(l=120 if horizontal else 20, r=20, t=20, b=20))
     fig.update_xaxes(title=None, showgrid=True, gridwidth=1, gridcolor='lightgray')
     fig.update_yaxes(title=None, showgrid=True, gridwidth=1, gridcolor='lightgray')
+    fig.update_layout(title=title,title_x=0.5, margin=dict(l=10, r=10, t=40, b=10)
+    )
     return fig
 # ---------------- HELPER FUNCTIONS ----------------
 def filter_top_n(df, row_col, col_col, top_n=None):
