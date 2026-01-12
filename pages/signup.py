479da6ae629ed8dfff987cec0143d599e4c29124
kyalorichard <kyalorichard11@gmail.com>
2026-01-12 05:47:44 -0300

    Update signup.py

--- pages/signup.py
+++ pages/signup.py
@@ -1,17 +1,21 @@
 import streamlit as st
-from auth.auth_db import create_user
+from auth.db import create_user
 from auth.security import hash_password
 
-st.set_page_config(page_title="Request Access", layout="centered")
 
-st.title("Request Dashboard Access")
+st.set_page_config(page_title="Signup", layout="centered")
+
+
+st.title("Request Access")
+
 
 email = st.text_input("Email")
 password = st.text_input("Password", type="password")
 
-if st.button("Submit Request"):
-    try:
-        create_user(email, hash_password(password))
-        st.success("Account created. Await admin approval.")
-    except:
-        st.error("Account already exists.")
+
+if st.button("Submit"):
+try:
+create_user(email, hash_password(password))
+st.success("Account created. Verify email and await admin approval.")
+except Exception:
+st.error("Account already exists")
