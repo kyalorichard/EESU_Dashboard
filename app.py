63811ac41289683da20578c5dc1eb41eb65bff0e
kyalorichard <kyalorichard11@gmail.com>
2026-02-09 16:36:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1190,6 +1190,7 @@ if tab_name=="Overview":
     a4 = filtered_global.groupby(["alert-country","alert-impact"]).size().reset_index(name='count')
     r1c1,r1c2 = st.columns(2)
     r2c1,r2c2 = st.columns(2)
+    print(df_clean["enabling-principle"].unique())
     
     r1c1.plotly_chart(create_h_stacked_bar(a1,y="alert-type",x="count",color_col="alert-impact",title="Alert type distribution", horizontal=True),use_container_width=True,  key="tab1_chart1")
     
