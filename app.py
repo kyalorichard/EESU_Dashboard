8aa3c3312a052b34550db9641dd17854c2f18966
kyalorichard <kyalorichard11@gmail.com>
2026-02-13 08:15:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from pathlib import Path
 import streamlit.components.v1 as components
 import base64
 from auth import auth_ui
+import math
 
 st.set_page_config(page_title="EU SEE Dashboard", layout="wide")
 
