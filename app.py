329432ee85d63d74922baad9ccf8448a6b2e7811
kyalorichard <kyalorichard11@gmail.com>
2026-02-19 21:32:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1286,7 +1286,7 @@ with tab_overview:
   
     #r1c2.plotly_chart(create_h_stacked_bar(a2,y="enabling-principle",x="count",color_col="alert-impact",title="Alert distribution across enabling principles", horizontal=True),use_container_width=True,  key="tab1_chart2")
     
-    #if is_privileged():
+    if is_privileged():
         r2c1.plotly_chart(create_h_stacked_bar(a3,y="region",x="count",color_col="alert-impact",title="Alert distribution across regions", horizontal=False, normalize_labels=False),use_container_width=True,  key="tab1_chart3")
         r2c2.plotly_chart(create_h_stacked_bar(a4,y="alert-country",x="count",color_col="alert-impact",title="Alert distribution across countries", horizontal=False, normalize_labels=False),use_container_width=True,  key="tab1_chart4")
 
@@ -1306,13 +1306,14 @@ with tab_overview:
             .loc[:, filtered_global.columns.intersection(cols_rename_map.keys())]
             .rename(columns=cols_rename_map)
         )
-       
+
+    else:
+        st.info("Sign in with an authorized account to unlock additional detailed and disaggregated data.") 
         # ---------------- Tab two data preview ------------------
     
-        with st.expander("Summary Data preview"):
-            st.write(filtered_global_prev)  
-    #else:
-        #st.info("Sign in with an authorized account to unlock additional detailed and disaggregated data.") 
+    with st.expander("Summary Data preview"):
+        st.write(filtered_global_prev)  
+    
         
           
             
