0038b903818ccb1a3eb0f661eba210ea80834530
kyalorichard <kyalorichard11@gmail.com>
2026-02-19 23:25:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -431,19 +431,25 @@ def render_summary_cards(df, base_bar_height=25,show_breakdown=True):
     """
 
     # --- Monitored Countries ---
-    with col1:
-        value_display = (
-            f'<span style="font-size:36px; font-weight:bold; color:#008CAA; margin-top:5px;">{total_countries}</span>'
-            if is_privileged()
-            else '<span style="font-size:20px; font-weight:bold; color:#008CAA; margin-top:5px;">Available on Request</span>'
-        )
-        st.markdown(f"""
+    if is_privileged():
+        with col1:
+            st.markdown(f"""
         <div style="{card_style}">
             <div style="{icon_style}">🌍</div>
             <span style="font-size:16px; font-weight:600; color:#555;">Monitored Countries</span>
-            {value_display}
+            <span style="font-size:36px; font-weight:bold; color:#008CAA; margin-top:5px;">{total_countries}</span>
         </div>
         """, unsafe_allow_html=True)
+    else:
+        with col1:
+            st.markdown(f"""
+        <div style="{card_style}">
+            <div style="{icon_style}">🌍</div>
+            <span style="font-size:16px; font-weight:600; color:#555;">Monitored Countries</span>
+            <span style="font-size:20px; font-weight:bold; color:#008CAA; margin-top:5px;">Available on Request</span>
+        </div>
+        """, unsafe_allow_html=True)
+            
 
     # --- Total Alerts ---
     with col2:
@@ -1290,33 +1296,33 @@ with tab_overview:
   
     #r1c2.plotly_chart(create_h_stacked_bar(a2,y="enabling-principle",x="count",color_col="alert-impact",title="Alert distribution across enabling principles", horizontal=True),use_container_width=True,  key="tab1_chart2")
     
-    if is_privileged():
-        r2c1.plotly_chart(create_h_stacked_bar(a3,y="region",x="count",color_col="alert-impact",title="Alert distribution across regions", horizontal=False, normalize_labels=False),use_container_width=True,  key="tab1_chart3")
-        r2c2.plotly_chart(create_h_stacked_bar(a4,y="alert-country",x="count",color_col="alert-impact",title="Alert distribution across countries", horizontal=False, normalize_labels=False),use_container_width=True,  key="tab1_chart4")
+    #if is_privileged():
+    r2c1.plotly_chart(create_h_stacked_bar(a3,y="region",x="count",color_col="alert-impact",title="Alert distribution across regions", horizontal=False, normalize_labels=False),use_container_width=True,  key="tab1_chart3")
+    r2c2.plotly_chart(create_h_stacked_bar(a4,y="alert-country",x="count",color_col="alert-impact",title="Alert distribution across countries", horizontal=False, normalize_labels=False),use_container_width=True,  key="tab1_chart4")
 
-        
-        cols_rename_map  = {
-            "post_title": "Title of post",
-            "summary": "Event summary",
-            "creation_date": "Date of submission",
-            "alert-country": "Country",
-            "enabling-principle": "Enabling principles",
-            "alert-impact": "Impact of alert",
-            "alert-type": "Type of alert"
-        }
-        # keep only existing columns, then rename
-        filtered_global_prev = (
-            filtered_global
-            .loc[:, filtered_global.columns.intersection(cols_rename_map.keys())]
-            .rename(columns=cols_rename_map)
-        )
-       
-        # ---------------- Tab two data preview ------------------
     
-        with st.expander("Summary Data preview"):
-            st.write(filtered_global_prev)  
-    else:
-        st.info("Sign in with an authorized account to unlock additional detailed and disaggregated data.")   
+    cols_rename_map  = {
+        "post_title": "Title of post",
+        "summary": "Event summary",
+        "creation_date": "Date of submission",
+        "alert-country": "Country",
+        "enabling-principle": "Enabling principles",
+        "alert-impact": "Impact of alert",
+        "alert-type": "Type of alert"
+    }
+    # keep only existing columns, then rename
+    filtered_global_prev = (
+        filtered_global
+        .loc[:, filtered_global.columns.intersection(cols_rename_map.keys())]
+        .rename(columns=cols_rename_map)
+    )
+    
+    # ---------------- Tab two data preview ------------------
+
+    with st.expander("Summary Data preview"):
+        st.write(filtered_global_prev)  
+    #else:
+       # st.info("Sign in with an authorized account to unlock additional detailed and disaggregated data.")   
             
 # ---------------- Negative Events ----------------
 with tab_negative:
@@ -1561,11 +1567,11 @@ with tab_negative:
         )
             
         # ---------------- Tab two data preview ----------------
-        if is_privileged():        
-            with st.expander("Summary Data preview"):
-                st.write(reactive_df_updated_prev)
-        else:
-            st.info("Sign in with an authorized account to unlock additional detailed and disaggregated data.")      
+        #if is_privileged():        
+        with st.expander("Summary Data preview"):
+            st.write(reactive_df_updated_prev)
+        #else:
+            #st.info("Sign in with an authorized account to unlock additional detailed and disaggregated data.")      
         
         # ---------------- TAB 3 (MAP) ----------------
 with tab_map:
