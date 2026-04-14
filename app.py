cee216c20d3e9c3157b2f300341ebfda08436a58
kyalorichard <kyalorichard11@gmail.com>
2026-04-14 15:39:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -164,6 +164,10 @@ def load_data():
     # Clean country names
     df['alert-country'] = df['alert-country'].replace({"Lebanon NAR": "Lebanon"})
 
+    # ❗ REMOVE alert-type == "event"
+    df = df[df['alert-type'].str.lower() != "event"]
+
+
     # Clean Actor of repression
     df['Actor of repression'] = df['Actor of repression'].astype(str).str.strip()
     df['Actor of repression'] = df['Actor of repression'].replace({"VNSAs": "Violent Non-State Actors"})
