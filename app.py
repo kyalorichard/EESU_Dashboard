3d0033b6f8954460ae6854aaf62ec4731df3a1bc
kyalorichard <kyalorichard11@gmail.com>
2026-04-30 09:02:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1379,7 +1379,7 @@ with tab_overview:
   
     #r1c2.plotly_chart(create_h_stacked_bar(a2,y="enabling-principle",x="count",color_col="alert-impact",title="Alert distribution across enabling principles", horizontal=True),use_container_width=True,  key="tab1_chart2")
     
-    if is_privileged():
+    #if is_privileged():
         r2c1.plotly_chart(create_h_stacked_bar(a3,y="region",x="count",color_col="alert-impact",title="Alert distribution across regions", horizontal=False, normalize_labels=False),use_container_width=True,  key="tab1_chart3")
         r2c2.plotly_chart(create_h_stacked_bar(a4,y="alert-country",x="count",color_col="alert-impact",title="Alert distribution across countries", horizontal=False, normalize_labels=False),use_container_width=True,  key="tab1_chart4")
 
@@ -1404,8 +1404,8 @@ with tab_overview:
     
         with st.expander("Summary Data preview"):
             st.write(filtered_global_prev)  
-    else:
-        st.info("Sign in with an authorized account to unlock additional detailed and disaggregated data.")   
+    #else:
+        #st.info("Sign in with an authorized account to unlock additional detailed and disaggregated data.")   
             
 # ---------------- Negative Events ----------------
 with tab_negative:
