514795f47a705ea9d1b5397a1df48f4f70c206f9
kyalorichard <kyalorichard11@gmail.com>
2026-06-12 10:44:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2714,7 +2714,7 @@ st.markdown(f"""
 <style>
 .dashboard-title-shell {{
     overflow: hidden;
-    margin-top: -9.5rem !important;
+    margin-top: -11.5rem !important;
     padding-top: 0rem !important;
     margin-bottom: 0.4rem !important;
 }}
