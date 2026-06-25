b109edecdee994472fa2056da513f125fc97a28c
kyalorichard <kyalorichard11@gmail.com>
2026-06-25 12:29:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import plotly.graph_objects as go
 import base64
 import hashlib
 from datetime import datetime
-from auth import auth_ui, is_privileged, is_authenticated, init_session, restore_session, logout
+#from auth import auth_ui, is_privileged, is_authenticated, init_session, restore_session, logout
 import math
 import paramiko
 import logging
@@ -21,6 +21,16 @@ import re
 import requests
 
 import uuid
+
+import warnings
+from streamlit.elements.lib.policies import CachedWidgetWarning
+
+warnings.filterwarnings(
+    "ignore",
+    category=CachedWidgetWarning
+)
+
+from auth import auth_ui, is_privileged, is_authenticated, init_session, restore_session, logout
 
 
 st.set_page_config(page_title="EUSEE Dashboard", layout="wide", initial_sidebar_state="collapsed")
