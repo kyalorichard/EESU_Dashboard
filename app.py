a1451a6f754f799cf97d58a2721fcc0f723d7684
kyalorichard <kyalorichard11@gmail.com>
2026-07-27 12:02:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5581,8 +5581,8 @@ def render_chart_shell():
 render_chart_shell()
 
 # ---------------- PERCENT AXIS / STANDARD HEIGHT HELPERS ----------------
-CHART_HEIGHT_VERTICAL = 420
-CHART_HEIGHT_HORIZONTAL = 420
+CHART_HEIGHT_VERTICAL = 430
+CHART_HEIGHT_HORIZONTAL = 430
 
 
 def _nice_percent_axis_max(max_pct):
